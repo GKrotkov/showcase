a82d3afec2f985658ccdd5c2586dd586b4248033
Gabriel Krotkov <GKrotkov@users.noreply.github.com>
2025-12-27 14:14:19 -0500

    Add files via upload

--- crescendo_avgs.R
+++ crescendo_avgs.R
@@ -4,21 +4,34 @@ rm(list = ls())
 library(scoutR)
 library(tidyverse)
 
+# Let's assume we're interested in figuring out what would be required to be 
+# in the top eighth - because there is one winning playoffs alliance 
+t8th_pct <- 1 - (1 / 8)
+b8th_pct <- 1 / 8
+z_crit <- pnorm(t8th_pct)
+
 ########################
 #### Data Wrangling ####
 ########################
 
-week_keys <- function(wk){
-    wk <- wk - 1 # tba uses 0-indexing
+week_keys <- function(wk, level = c("qualifier", "dcmp", "cmp")){
+    level <- match.arg(level)
+    level_code <- switch(
+        level, 
+        qualifier = c(0, 1), dcmp = c(2, 5), cmp = c(3, 4)
+    )
     officials <- events(year = 2024, official = TRUE)
     keys <- officials |>
-        filter(week == wk, event_type %in% c(0, 1)) |>
+        filter(
+            (week == wk - 1) | level == "cmp", # tba uses 0-indexing
+            event_type %in% level_code
+        ) |>
         pull(key)
     return(keys)
 }
 
-matches_avg_notes <- function(match_data){
-    match_data <- match_data |>
+count_alliance_notes <- function(match_data){
+    result <- match_data |>
         mutate(
             red_notes = red_teleopSpeakerNoteCount + red_teleopAmpNoteCount +
                 red_teleopSpeakerNoteAmplifiedCount + red_autoAmpNoteCount + 
@@ -26,70 +39,82 @@ matches_avg_notes <- function(match_data){
             blue_notes = blue_teleopSpeakerNoteCount + blue_teleopAmpNoteCount +
                 blue_teleopSpeakerNoteAmplifiedCount + blue_autoAmpNoteCount + 
                 blue_autoSpeakerNoteCount + (blue_endGameNoteInTrapPoints / 5)
+        ) |>
+        select(
+            key, red_notes, blue_notes
         )
 
-    result <- c(
-        "mean" = mean(c(match_data$red_notes, match_data$blue_notes)),
-        "sd" = sd(c(match_data$red_notes, match_data$blue_notes))
+    return(result)
+}
+
+week_count_notes <- function(week, level = "qualifier"){
+    keys <- week_keys(week, level = level)
+    match_data <- lapply(keys, event_matches, match_type = "all")
+    match_data <- bind_rows(match_data)
+    
+    qual_matches <- match_data |>
+        filter(comp_level == "qm") |>
+        count_alliance_notes()
+
+    playoff_matches <- match_data |>
+        filter(comp_level != "qm") |>
+        count_alliance_notes()
+
+    # uses vector recycling to match keys to matches
+    result <- rbind(
+        data.frame(week = week, type = "qual", key = qual_matches$key, 
+                   level = level, notes = c(qual_matches$red_notes, 
+                                            qual_matches$blue_notes)), 
+        data.frame(week = week, type = "playoff", key = playoff_matches$key,
+                   level = level, notes = c(playoff_matches$red_notes, 
+                                            playoff_matches$blue_notes))
     )
     return(result)
 }
 
-week_alliance_avg_notes <- function(week){
-    keys <- week_keys(week)
+###################
+#### API Calls ####
+###################
 
-    match_data <- lapply(keys, event_matches, match_type = "all")
-    match_data <- bind_rows(match_data)
+# refactored from previous approach so we can retain the data (reduce the 
+# amount we're bugging TBA), and do more analysis than just avg/sd
 
-    qual_matches <- match_data |>
-        filter(comp_level == "qm")
-
-    playoff_matches <- match_data |>
-        filter(comp_level != "qm")
-
-    result <- c(
-        matches_avg_notes(qual_matches),
-        matches_avg_notes(playoff_matches)
-    )
-
-    names(result) <- c("qual mean", "qual sd", "playoff mean", "playoff sd")
-
-    return(round(result, digits = 2))
-}
-
-qual_avg <- rep(NA, 6)
-names(qual_avg) <- paste("Week", 1:6)
-
-qual_sd <- rep(NA, 6)
-names(qual_sd) <- paste("Week", 1:6)
-
-playoff_avg <- rep(NA, 6)
-names(playoff_avg) <- paste("Week", 1:6)
-
-playoff_sd <- rep(NA, 6)
-names(playoff_sd) <- paste("Week", 1:6)
-
+result <- list()
 for (i in 1:6){
-    tmp <- week_alliance_avg_notes(i)
-    qual_avg[i] <- tmp[1]
-    qual_sd[i] <- tmp[2]
-    playoff_avg[i] <- tmp[3]
-    playoff_sd[i] <- tmp[4]
+    result[[i]] <- week_count_notes(i, level = "qualifier")
 }
+
+result[[7]] <- week_count_notes(6, level = "dcmp")
+result[[8]] <- week_count_notes(NA, level = "cmp")
+
+result <- bind_rows(result)
+
+result <- result |>
+    mutate(
+        stage = case_when(
+            level == "qualifier" ~ week, 
+            level == "dcmp" ~ 7, 
+            level == "cmp" ~ 8
+        )
+    )
 
 #######################
 #### Vizualization ####
 #######################
 
-viz <- tibble(week = 1:6, qual_avg = qual_avg, qual_sd = qual_sd, 
-              playoff_avg, playoff_sd = playoff_sd)
+week_labs <- c("Week 1", "Week 2", "Week 3", "Week 4", 
+               "Week 5", "Week 6", "DCMP", "CMP")
 
-viz <- viz |>
-    pivot_longer(-week, names_to = c("level", ".value"), names_sep = "_") |>
-    mutate(lower = avg - sd, upper = avg + sd, 
-           level = factor(level, levels = c("qual", "playoff")))
+viz <- result |>
+    group_by(type, stage) |>
+    summarize(avg = round(mean(notes), 2), sd = round(sd(notes), 2), 
+              median = median(notes), t8th = quantile(notes, t8th_pct), 
+              b8th = quantile(notes, b8th_pct)) |>
+    mutate(type = factor(type, levels = c("qual", "playoff")), 
+           stage = factor(stage, levels = 1:8, labels = week_labs), 
+           lower = avg - sd, upper = avg + sd)
 
-ggplot(viz, aes(x = week, y = avg, color = level)) +
+ggplot(viz, aes(x = stage, y = avg, color = type, group = type)) +
     geom_errorbar(aes(ymin = lower, ymax = upper), 
                   width = 0.5, position = "dodge", 
                   linewidth = 0.75) +
@@ -97,10 +122,24 @@ ggplot(viz, aes(x = week, y = avg, color = level)) +
               linewidth = 0.75) +
     geom_point(position = position_dodge(width = 0.5)) +
     geom_hline(yintercept = 18, lty = 2, col = "black") + 
-    annotate("text", x = 1.5, y = 19, label = "Melody") +
+    annotate("text", x = 1.6, y = 19, label = "Melody") +
     labs(title = "How many notes did alliances score in Crescendo?",
          subtitle = "Bounds show +/- 1 SD",
          x = "Week", y = "Average Notes") +
+    theme_bw()
+
+ggplot(viz, aes(x = stage, y = median, color = type, group = type)) +
+    geom_errorbar(aes(ymin = b8th, ymax = t8th), 
+                  width = 0.5, position = "dodge", 
+                  linewidth = 0.75) +
+    geom_line(position = position_dodge(width = 0.5), 
+              linewidth = 0.75) +
+    geom_point(position = position_dodge(width = 0.5)) +
+    geom_hline(yintercept = 18, lty = 2, col = "black") + 
+    annotate("text", x = 1.6, y = 19, label = "Melody") +
+    labs(title = "How many notes did alliances score in Crescendo?",
+         subtitle = "Bounds show [12.5%, median, 87.5%]",
+         x = "Week", y = "Median Notes") +
     theme_bw()
 
 ########################
